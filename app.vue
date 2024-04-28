a7a33ecd11f371d00097b977c29276ff81cfbe8b
Тихомиров Григорий <gptikhomi1@mts.ru>
2024-04-28 23:53:55 +0300

    init project

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <the-header />
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+.app {
+
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="flex">Groshev housing estate</h1>
+  </div>
+</template>
