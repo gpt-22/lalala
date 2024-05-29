7576111167267a4d23f7da9de8f51eec24177f97
Тихомиров Григорий <gptikhomi1@mts.ru>
2024-05-29 04:07:42 +0300

    asdf

--- composables/useVideo.data.js
+++ composables/useVideo.data.js
@@ -33,7 +33,7 @@ export const allVideos = [
     prevKey: '',
     key: '1',
     nextKey: '2',
-    src: `/videos/01_01.mp4`,
+    src: `/videos/01_01.mov`,
     // src: `${VIDEO_BASE_URL}/01_01.mp4`,
     loadKeys: ['2', '3', '4', '5']
   },
@@ -42,7 +42,7 @@ export const allVideos = [
     prevKey: '1',
     key: '2',
     nextKey: '3',
-    src: `/videos/01_04_mov.mov`,
+    src: `/videos/01_02.mov`,
     // src: `${VIDEO_BASE_URL}/01_02.mp4`, //kek_0-1.mp4`,
     isTransition: true,
     loadKeys: ['3', '1']
@@ -52,7 +52,7 @@ export const allVideos = [
     prevKey: '2',
     key: '3',
     nextKey: '4',
-    src: `/videos/01_03.mp4`,
+    src: `/videos/01_03.mov`,
     // src: `${VIDEO_BASE_URL}/01_03.mp4`, // kek_1-1.mp4`,
     section: 'section-1',
     loadKeys: ['4', '2']
@@ -62,7 +62,7 @@ export const allVideos = [
     prevKey: '3',
     key: '4',
     nextKey: '5',
-    src: `/videos/01_04.mp4`,
+    src: `/videos/01_04.mov`,
     // src: `${VIDEO_BASE_URL}/01_04.mp4`, // kek_1-2.mp4`,
     isTransition: true,
     loadKeys: ['5', '3']
@@ -72,7 +72,7 @@ export const allVideos = [
     prevKey: '5',
     key: '4r',
     nextKey: '3',
-    src: `/videos/01_04_reverse.mp4`,
+    src: `/videos/01_04_reverse.mov`,
     // src: `${VIDEO_BASE_URL}/01_04_reverse.mp4`, // kek_2-1.mp4`,
     isTransition: true,
     loadKeys: ['3', '5']
@@ -82,7 +82,7 @@ export const allVideos = [
     prevKey: '4r',
     key: '5',
     nextKey: '6',
-    src: `/videos/01_05.mp4`,
+    src: `/videos/01_05.mov`,
     // src: `${VIDEO_BASE_URL}/01_05.mp4`, // kek_2-2.mp4`,
     section: 'section-2',
     loadKeys: ['6', '4r']
@@ -92,7 +92,7 @@ export const allVideos = [
     prevKey: '5',
     key: '6',
     nextKey: '7',
-    src: `/videos/01_06.mp4`,
+    src: `/videos/01_06.mov`,
     // src: `${VIDEO_BASE_URL}/01_06.mp4`,
     isTransition: true,
     loadKeys: ['7', '5']
@@ -102,7 +102,7 @@ export const allVideos = [
     prevKey: '7',
     key: '6r',
     nextKey: '5',
-    src: `/videos/01_06_reverse.mp4`,
+    src: `/videos/01_06_reverse.mov`,
     // src: `${VIDEO_BASE_URL}/01_06_reverse.mp4`,
     isTransition: true,
     loadKeys: ['5', '7']
@@ -112,7 +112,7 @@ export const allVideos = [
     prevKey: '6r',
     key: '7',
     nextKey: '8',
-    src: `/videos/01_07.mp4`,
+    src: `/videos/01_07.mov`,
     // src: `${VIDEO_BASE_URL}/01_07.mp4`,
     section: 'section-3',
     loadKeys: ['8', '6r']
@@ -122,7 +122,7 @@ export const allVideos = [
     prevKey: '5',
     key: '8',
     nextKey: '9',
-    src: `/videos/01_08.mp4`,
+    src: `/videos/01_08.mov`,
     // src: `${VIDEO_BASE_URL}/01_08.mp4`,
     isTransition: true,
     loadKeys: ['9', '5']
@@ -132,7 +132,7 @@ export const allVideos = [
     prevKey: '9',
     key: '8r',
     nextKey: '5',
-    src: `/videos/01_08_reverse.mp4`,
+    src: `/videos/01_08_reverse.mov`,
     // src: `${VIDEO_BASE_URL}/01_08_reverse.mp4`,
     isTransition: true,
     loadKeys: ['5', '9']
@@ -142,7 +142,7 @@ export const allVideos = [
     prevKey: '8r',
     key: '9',
     nextKey: '',
-    src: `/videos/01_09.mp4`,
+    src: `/videos/01_09.mov`,
     // src: `${VIDEO_BASE_URL}/01_09.mp4`,
     section: 'section-4',
     loadKeys: ['8r']
