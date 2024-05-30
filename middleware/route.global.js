7e40da8402139d4d67213002afe28e8d2e35f3e8
Тихомиров Григорий <gptikhomi1@mts.ru>
2024-05-30 22:50:21 +0300

    asf

--- middleware/route.global.js
+++ middleware/route.global.js
@@ -2,8 +2,8 @@ export default defineNuxtRouteMiddleware((to, from, kek) => {
   const { width } = useWindowSize()
   const { unavailableReason } = useShow()
 
-  const isChrome = navigator.userAgent.indexOf('Chrome') != -1
-  const isFirefox = navigator.userAgent.indexOf('Firefox') != -1
+  const isChrome = navigator.userAgent.indexOf('Chrome') !== -1
+  const isFirefox = navigator.userAgent.indexOf('Firefox') !== -1
 
   if (to.name === 'cinematic') {
     return
@@ -12,14 +12,10 @@ export default defineNuxtRouteMiddleware((to, from, kek) => {
   if (!isChrome && !isFirefox) {
     unavailableReason.value = 'Браузер не поддерживается :('
     setPageLayout('inappropriate-device')
-  }
-
-  if (width.value < 1024 && to.name !== 'cinematic') {
+  } else if (width.value < 1024 && to.name !== 'cinematic') {
     unavailableReason.value = 'Cайт доступен только на больших разрешениях экрана.'
     setPageLayout('inappropriate-device')
-  }
-
-  if (to.matched.length === 0) {
+  } else if (to.matched.length === 0) {
     console.log('navigateTo /')
     return navigateTo('/')
   }
